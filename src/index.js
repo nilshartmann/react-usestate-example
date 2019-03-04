1e8858e545b0df2601d3b166bbaf7606234e02de
Nils Hartmann <nils@nilshartmann.net>
2019-03-04 19:37:09 +0100

    example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,105 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { useState, useEffect } from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => (
+  <div style={{ width: "100%" }}>
+    <div>
+      How is react? <TextInput />
+    </div>
+    <div>(Hint: React is pretty awsome, could say react rocks.)</div>
+  </div>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function useChangeHandler(initialValue) {
+  const [state, setState] = useState(initialValue);
+  const [verificationState, setVerificationState] = useState(null);
+
+  useEffect(() => {
+    console.log(
+      `useServiceVerification. Create new ServerRequest for '${state}'`
+    );
+
+    // remove old state
+    setVerificationState(null);
+
+    const request = new ServerRequest(state);
+    request.verifyValue().then(response =>
+      setVerificationState({
+        message: response,
+        requestId: request.requestId
+      })
+    );
+
+    //
+    return function() {
+      console.log("useEffect CANCEL REQUEST " + request.requestId);
+      request.cancel();
+    };
+  }, [state]);
+
+  return [state, e => setState(e.target.value), verificationState];
+}
+
+let requestDebugCounter = 0;
+/**
+ *  Represents a running server validation request.
+ * could be modeled without class, but too lazy today 😴
+ */
+class ServerRequest {
+  constructor(value) {
+    this.requestId = `ServerRequest#${++requestDebugCounter}_for_value_'${value}'`;
+
+    this.canceled = false;
+    this.value = value;
+  }
+
+  verifyValue() {
+    // simulate real server access
+    return new Promise(resolve =>
+      window.setTimeout(() => {
+        // Response from server received!
+
+        if (this.canceled) {
+          // we're canceled... discard everything
+          console.log(
+            `[${this.requestId}] received Response for out-of-date value '${
+              this.value
+            }' but has been canceled. Discarding`
+          );
+
+          return;
+        }
+        console.log(
+          `[${this.requestId}] received Response for up-to-date value '${
+            this.value
+          }'.`
+        );
+        resolve(this.value === "react rocks" ? "YES!" : "NO. TRY AGAIN.");
+      }, 1500)
+    );
+  }
+
+  cancel() {
+    console.log(`[${this.requestId}] canceling request`);
+    this.canceled = true;
+  }
+}
+
+function TextInput() {
+  const [state, setState, verificationState] = useChangeHandler("");
+
+  return (
+    <>
+      <input value={state} onChange={e => setState(e)} />
+      {verificationState && (
+        <div>
+          Server returned: <b>{verificationState.message}</b> (response coming
+          from server request <code>{verificationState.requestId}</code>)
+        </div>
+      )}
+    </>
+  );
+}
+
+render(<App />, document.getElementById("root"));
